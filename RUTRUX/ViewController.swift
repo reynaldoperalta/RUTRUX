90ea5083eb627db83507d381d5cabb125ce85799
reynaldoperalta <reynaldopema14@gmail.com>
2021-11-18 15:59:23 -0800

    Initial Commit

---
+++ RUTRUX/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RUTRUX
+//
+//  Created by reynaldo peralta marquez on 11/18/21.
+//  Copyright © 2021 reynaldo peralta marquez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
